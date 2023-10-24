e1c437f1fdcdb1399863b545634b323557b2b6a1
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-10-24 17:37:02 +0800

    completed DeleteMiddleOfNodeOfLinkedlist

---
+++ DeleteMiddleNodeOfLinkedlist.java
@@ -0,0 +1,16 @@
+public class DeleteMiddleNodeOfLinkedlist {
+  public static void main(String[] args) {
+
+  }
+
+  public static ListNode solution(ListNode head) {
+    ListNode slow = head.next, fast = head.next.next;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return head;
+  }
+}
