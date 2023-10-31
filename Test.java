b64d6019dc36e44db03d97b39396bd248e0106c4
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-10-31 17:00:12 +0800

    completed validateIPAddress.js

--- Test.java
+++ Test.java
@@ -1,4 +1,13 @@
+import java.util.Arrays;
 
-public @interface Test {
+public class Test {
+  public static void main(String[] args) {
+    int[] arr1 = {1,2,3,4};
+    int[] arr2 = arr1;
+    arr1[0] = 5;
+    System.out.println(Arrays.toString(arr2));
+    System.out.println(Arrays.toString(arr1));
+    (a,b) -> 
+  }
 
 }
---
+++ tes.js
@@ -0,0 +1,7 @@
+let arr1 = [1,2,3]
+let arr2 = arr1;
+
+arr2.push(4);
+
+console.log(arr2)
+console.log(arr1)
