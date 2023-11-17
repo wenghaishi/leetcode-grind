cf891566d0278bdbafb892ab3ef217b3c5061de1
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-11-17 14:47:51 +0800

    completed BestTimeToBuyAndSellStockTwo

---
+++ BestTimeToBuyAndSellStockTwo.java
@@ -0,0 +1,22 @@
+public class BestTimeToBuyAndSellStockTwo {
+  public static void main(String[] args) {
+    int[] prices1 = { 1, 2, 3 };
+    int[] prices2 = { 7, 1, 5, 3, 6, 4 };
+    int[] prices3 = { 5, 1 };
+
+    System.out.println(solution(prices1));
+    System.out.println(solution(prices2));
+    System.out.println(solution(prices3));
+
+  }
+
+  public static int solution(int[] prices) {
+    int profit = 0;
+    for (int i = 1; i < prices.length; i++) {
+      if (prices[i] - prices[i - 1] > 0) {
+        profit += prices[i] - prices[i - 1];
+      }
+    }
+    return profit;
+  }
+}
