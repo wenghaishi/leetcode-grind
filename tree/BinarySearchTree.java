21849b928e7f225a34c53f71e43fcf49f65d3ab2
weng haishi <wenghaishi1@gmail.com>
2023-11-22 12:24:39 +0800

    completed insert method for BinarySearchTree

---
+++ tree/BinarySearchTree.java
@@ -0,0 +1,50 @@
+package tree;
+
+public class BinarySearchTree {
+
+    Node root;
+
+    static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    public BinarySearchTree(int value) {
+        Node newNode = new Node(value);
+        root = newNode;
+
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        Node temp = root;
+
+        while (true) {
+            if (newNode.value > temp.value) {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                } else {
+                    temp = temp.right;
+                }
+            } else if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                } else {
+                    temp = temp.left;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+}
