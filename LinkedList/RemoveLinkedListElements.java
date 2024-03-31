5a3250745efb74cf1e93b268eb9aa9b44a2a22a5
weng haishi <wenghaishi1@gmail.com>
2024-03-31 19:57:11 +0800

    completed RemoveLinkedListElements

---
+++ LinkedList/RemoveLinkedListElements.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class RemoveLinkedListElements {
+    public static void main(String args[]) {
+
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return dummy.next;
+        
+    }
+    
+}
