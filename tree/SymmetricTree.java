7e3fce5537bc9dde20bb22515637f633955f898d
weng haishi <wenghaishi1@gmail.com>
2024-05-14 12:26:10 +0800

    completed majority element

---
+++ tree/SymmetricTree.java
@@ -0,0 +1,13 @@
+package tree;
+
+import javax.swing.tree.TreeNode;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        
+    }
+}
