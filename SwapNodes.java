a26cd8d26d1cf2094ac71751647843ef83619e38
weng haishi <wenghaishi1@gmail.com>
2024-10-08 23:45:07 +0800

    minor changes

---
+++ SwapNodes.java
@@ -0,0 +1,19 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class SwapNodes {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public ListNode solution(ListNode header) {
+        return null;
+    }
+}
