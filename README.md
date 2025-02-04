513be781dd7d3d496186de1a160ef660316a9138
weng haishi <wenghaishi1@gmail.com>
2025-02-04 23:49:03 +0800

    modify readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Daily grind for leetcode
 
 ...ok maybe not daily...
+
+Python, Java, Javascript and Go
--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-let n = 5
-if ((n & 1) === 0) {
-  console.log(n + " is even");
-} else {
-  console.log(n + " is odd");
-}
+const a = ",hi my name is"
+
+const b = 'hi my'
+
+const c = "hi my name is"
+
+const d = "hi. ,whats up?"
+
+const e = d.replace(/\. ,/g, '. ')
+
+console.log(a.includes(b))
+console.log(a.includes(c))
+
+console.log(e)
