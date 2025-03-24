9f465ca3f165e50bff348c5f305ae758f255f087
weng haishi <wenghaishi1@gmail.com>
2025-03-24 18:43:49 +0800

    completed

--- python/70.py
+++ python/70.py
@@ -16,3 +16,24 @@ def solution(n):
     helper(0)
 
     return count
+
+def cached(n):
+    cached = {}
+
+    def helper(current):
+        if current == n:
+            return 1
+        if current > n:
+            return 0
+        if current in cached:
+            return cached[current]
+        else:
+            current_count = helper(current + 1) + helper(current + 2)
+            cached[current] = current_count
+            return current_count
+
+    solution = helper(0)
+
+    return solution
+
+
