7f183a3aebc8a92c4eb49928ef8464fc0161f873
weng haishi <wenghaishi1@gmail.com>
2025-08-11 16:35:15 +0800

    completed sql qn

---
+++ sql/1068.sql
@@ -0,0 +1,36 @@
+-- +-------------+-------+
+-- | Column Name | Type  |
+-- +-------------+-------+
+-- | sale_id     | int   |
+-- | product_id  | int   |
+-- | year        | int   |
+-- | quantity    | int   |
+-- | price       | int   |
+-- +-------------+-------+
+-- (sale_id, year) is the primary key (combination of columns with unique values) of this table.
+-- product_id is a foreign key (reference column) to Product table.
+-- Each row of this table shows a sale on the product product_id in a certain year.
+-- Note that the price is per unit.
+ 
+
+-- Table: Product
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | product_id   | int     |
+-- | product_name | varchar |
+-- +--------------+---------+
+-- product_id is the primary key (column with unique values) of this table.
+-- Each row of this table indicates the product name of each product.
+ 
+
+-- Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+
+-- Return the resulting table in any order.
+
+
+select P.product_name, S.year, S.price
+FROM  Sales S
+JOIN Product P
+ON P.product_id = S.product_id;
