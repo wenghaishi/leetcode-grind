69592a24236c6b1d71c0915fe5fd0fedfbe3cb67
weng haishi <wenghaishi1@gmail.com>
2025-08-11 16:51:24 +0800

    completed qn

---
+++ sql/1581.sql
@@ -0,0 +1,35 @@
+-- Table: Visits
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | visit_id    | int     |
+-- | customer_id | int     |
+-- +-------------+---------+
+-- visit_id is the column with unique values for this table.
+-- This table contains information about the customers who visited the mall.
+ 
+
+-- Table: Transactions
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | transaction_id | int     |
+-- | visit_id       | int     |
+-- | amount         | int     |
+-- +----------------+---------+
+-- transaction_id is column with unique values for this table.
+-- This table contains information about the transactions made during the visit_id.
+ 
+
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+
+SELECT V.customer_id, COUNT(*) AS count_no_trans
+FROM Visits V
+LEFT JOIN Transactions T
+ON T.visit_id = V.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY 
+  customer_id
