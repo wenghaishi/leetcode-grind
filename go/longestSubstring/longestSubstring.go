998ded91e05348508076e889a90619efb64c85a9
weng haishi <wenghaishi1@gmail.com>
2025-10-27 17:51:08 +0800

    complete the brute force solution of longestSubstring

---
+++ go/longestSubstring/longestSubstring.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func lss(s string) int {
+	stringArr := strings.Split(s, "")
+	maxLength := 0
+	
+	for i := 0; i < len(stringArr); i++ {
+		mySet := make(map[string]struct{})
+		currentLength := 0
+
+		for j := i; j < len(stringArr); j++ {
+			_, exists := mySet[stringArr[j]]
+
+			if exists {
+				break
+			} else {
+				mySet[stringArr[j]] = struct{}{}
+				currentLength++
+				if currentLength > maxLength {
+					maxLength = currentLength
+				}
+			}
+		}
+	}
+
+	return maxLength
+}
+
+func main() {
+	test1 := "abcdab"
+
+	fmt.Println(lss(test1))
+
+	test2 := "aaaaaa"
+
+	fmt.Println(lss(test2))
+} 
