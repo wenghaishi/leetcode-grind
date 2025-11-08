4cab2c166af3bd4e7186d9b681bbe10d9184ff55
weng haishi <wenghaishi1@gmail.com>
2025-11-08 21:23:48 +0800

    adjust solution to romanToNumber

--- go/romanToNumber/romanToNumber.go
+++ go/romanToNumber/romanToNumber.go
@@ -4,7 +4,7 @@ func romanToNumber(s string) int {
 	values := map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 	var number int
 	for i := 0; i < len(s); i++ {
-		if i + 1 < len(s) && values[s[i]] < values[s[i]] {
+		if i + 1 < len(s) && values[s[i]] < values[s[i + 1]] {
 			number -= values[s[i]]
 		} else {
 			number += values[s[i]]
