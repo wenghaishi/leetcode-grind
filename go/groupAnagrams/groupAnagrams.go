adf8eb61f6d1d2d7ec885797078c61600b37f2b8
weng haishi <wenghaishi@wengs-MacBook-Air.local>
2026-02-09 13:29:36 +0800

    completed group anagrams

---
+++ go/groupAnagrams/groupAnagrams.go
@@ -0,0 +1,28 @@
+package main
+
+import "sort"
+
+func groupAnagrams(strs []string) [][]string {
+
+	strsMap := make(map[string][]string)
+
+	for _, v := range strs {
+		strRune := []rune(v)
+
+		sort.Slice(strRune, func(i,j int) bool {
+			return strRune[i] < strRune[j]
+		} )
+
+		strString := string(strRune)
+		strsMap[strString] = append(strsMap[strString], v)
+	}
+
+	var solution [][]string
+
+	for _, value := range strsMap {
+		solution = append(solution, value)
+	}
+
+	return solution
+
+}
