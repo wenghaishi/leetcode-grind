1cda34aaacc7ca9591b98b3a1180532b75b5d7fd
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2026-03-22 01:17:41 +0800

    completed mergeSort

--- go/mergeSort/mergeSort.go
+++ go/mergeSort/mergeSort.go
@@ -1,23 +1,43 @@
 package main
 
-func mergeSort(arr []int) []int {
-	length := len(arr)
-	if length <= 1 {
-		return
-	}
+import "fmt"
 
-	middle := length / 2
-
-	leftArr := arr[:middle]
-	rightArr := arr[middle:]
-
-	i,j := 0,0
-
-	for; i < length; i++ {
-
-	}
+func main() {
+	test1 := []int{10, 9, 1, 2, 3, 5, 7, 6, 4, 8}
+	res := mergeSort(test1)
+	fmt.Println(res)
 }
 
-func merge(leftArr []int, rightArr []int, arr []int) {
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
 
+	middle := len(arr) / 2
+	leftArr, rightArr := arr[:middle], arr[middle:]
+
+	leftArr = mergeSort(leftArr)
+	rightArr = mergeSort(rightArr)
+
+	return merge(leftArr, rightArr)
+}
+
+func merge(leftArr, rightArr []int) []int {
+	solution := make([]int, 0, len(leftArr)+len(rightArr))
+	l, r := 0, 0
+
+	for l < len(leftArr) && r < len(rightArr) {
+		if leftArr[l] < rightArr[r] {
+			solution = append(solution, leftArr[l])
+			l++
+		} else {
+			solution = append(solution, rightArr[r])
+			r++
+		}
+	}
+
+	solution = append(solution, leftArr[l:]...)
+	solution = append(solution, rightArr[r:]...)
+
+	return solution
 }
